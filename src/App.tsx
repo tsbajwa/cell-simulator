0c3eee4c549f31391c5dbf6cbf7cc746e48122de
Teranjit <teranjit@gmail.com>
2021-09-22 12:43:15 +1000

    feat: Added basic geenration functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import produce from "immer";
 /**
  *
  * Use React and TypeScript.
@@ -13,7 +13,7 @@ import React, { useState } from "react";
   User can make Cells "alive". DONE
   User can make Cells "dead". DONE
   User can trigger "next generation".
-  User can trigger a "reset" to the initial state.
+  User can trigger a "reset" to the initial state. [DONE]
   */
 
 // Initial empty board
@@ -21,6 +21,7 @@ import React, { useState } from "react";
 // Reset button - reset to initial state i.e all cells dead
 // Simulate button - logic to work out
 
+// GRID IS 0 based
 //TODO: cleanup. Be careful of referencing same array vs creating new
 const createInitialGrid = (numRows: number, numCols: number) => {
   const grid = [];
@@ -42,15 +43,75 @@ function App() {
     createInitialGrid(numRows, numCols)
   );
 
-  const toggleCellState = (x: number, y: number) => {
-    const newGrid = [...grid];
-    newGrid[x][y] = !newGrid[x][y];
+  const [generating, setGenerating] = useState(false);
 
-    setGrid(newGrid);
+  const toggleCellState = (x: number, y: number) => {
+    setGrid((grid) => {
+      return produce(grid, (newGrid) => {
+        newGrid[x][y] = !newGrid[x][y];
+        return newGrid;
+      });
+    });
   };
 
   const resetGrid = () => {
     setGrid(createInitialGrid(numRows, numCols));
+  };
+
+  //TODO: A Cell who "comes to life" outside the board should wrap at the other side of the board.
+  const getTotalAliveNeighbours = (grid: TGrid, x: number, y: number) => {
+    let totalAliveNeighbours = 0;
+
+    // offset
+    const neighbourRelativePositions = [
+      [0, 1],
+      [1, 1],
+      [1, 0],
+      [1, -1],
+      [0, -1],
+      [-1, -1],
+      [-1, 0],
+      [-1, 1],
+    ];
+
+    neighbourRelativePositions.forEach(([relativeX, relativeY]) => {
+      const neighbourX = x + relativeX;
+      const neighbourY = y + relativeY;
+      const neighbourValue = grid[neighbourX]?.[neighbourY];
+      // A true value signifies that neighbour is alive
+      neighbourValue && totalAliveNeighbours++;
+    });
+
+    return totalAliveNeighbours;
+  };
+
+  const startGeneration = () => {
+    setGrid((grid) => {
+      return produce(grid, (newGrid) => {
+        for (let x = 0; x < newGrid.length; x++) {
+          for (let y = 0; y < newGrid[x].length; y++) {
+            const currentCell = grid[x][y];
+            const aliveNeighbours = getTotalAliveNeighbours(grid, x, y);
+
+            if (currentCell) {
+              if (aliveNeighbours < 2) {
+                newGrid[x][y] = false;
+              }
+              if (aliveNeighbours === 2 || aliveNeighbours === 3) {
+                newGrid[x][y] = true;
+              }
+            } else {
+              if (aliveNeighbours === 3) {
+                console.log(x, y, currentCell, aliveNeighbours);
+
+                newGrid[x][y] = true;
+              }
+            }
+          }
+        }
+        return newGrid;
+      });
+    });
   };
 
   return (
@@ -79,6 +140,7 @@ function App() {
         )}
       </div>
       <span onClick={resetGrid}>Reset Button</span>
+      <span onClick={startGeneration}>Generate Button</span>
     </>
   );
 }
